4e2372b2d2db7191ec8a6cac24c3ccd0c67b8fda
atifsyedali <atif.ali@hpe.com>
2022-06-13 11:25:40 -0500

    CASMNET-1022 Remove UAI option for now

--- scripts/operations/pyscripts/pyscripts/commands/cmd_test_bican_external.py
+++ scripts/operations/pyscripts/pyscripts/commands/cmd_test_bican_external.py
@@ -42,7 +42,7 @@ import os
 )
 @click.option(
     "--to-types",
-    type=click.Choice(["ncn_master", "ncn_worker", "ncn_storage", "cn", "uan", "uai", "spine_switch", "leaf_switch", "leaf_BMC", "CDU"], case_sensitive=False),
+    type=click.Choice(["ncn_master", "ncn_worker", "ncn_storage", "cn", "uan", "spine_switch", "leaf_switch", "leaf_BMC", "CDU"], case_sensitive=False),
     multiple=True,
     default=["ncn_master", "cn", "uan", "spine_switch"],
     help="What types of nodes to test SSH access to. Defaults: ('ncn_master', 'cn', 'uan', 'spine_switch')"
